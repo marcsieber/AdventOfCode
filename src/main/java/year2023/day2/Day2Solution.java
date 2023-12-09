489e9897d0316f50cf0571f3fa730e82093598cf
marcsieber <ms-coding@outlook.com>
2023-12-09 15:21:24 +0100

    Day 2 second solved.

--- src/main/java/year2023/day2/Day2Solution.java
+++ src/main/java/year2023/day2/Day2Solution.java
@@ -21,14 +21,24 @@ public class Day2Solution {
     public static void main(String[] args) {
 
         List<String> input = Utils.readInputFromResources(inputFile);
+
         int sum = input.stream()
                 .map(Game::new)
                 .filter(game -> game.isPossible(target))
                 .mapToInt(Game::getGameId)
                 .sum();
 
-        System.out.println(sum);
+        System.out.println("Sume of GameIds: " +sum);
+
+        int sumPower = input.stream()
+                .map(Game::new)
+                .map(Game::minimumNeededCubes)
+                .mapToInt(GameSet::power)
+                .sum();
+
+        System.out.println("Sum of the power of all sets: " + sumPower);
     }
+
 
     private static class Game {
         private static final Pattern gameIdRegex = Pattern.compile("Game (\\d*)");
@@ -49,7 +59,17 @@ public class Day2Solution {
         private boolean isPossible(GameSet gameSet) {
             return sets.stream().allMatch(set -> set.isPossibleOutcome(gameSet));
         }
+
+        private GameSet minimumNeededCubes() {
+
+            int maxRed   = sets.stream().mapToInt(GameSet::getRed).max().orElse(0);
+            int maxGreen = sets.stream().mapToInt(GameSet::getGreen).max().orElse(0);
+            int maxBlue  = sets.stream().mapToInt(GameSet::getBlue).max().orElse(0);
+
+            return new GameSet(maxRed, maxGreen, maxBlue);
+        }
     }
+
 
     private static class GameSet {
 
@@ -71,14 +91,23 @@ public class Day2Solution {
 
         private GameSet(int red,  int green, int blue) {
             this.red = red;
-            this.blue = blue;
             this.green = green;
+            this.blue = blue;
+        }
+
+        public int getRed() {return red;}
+        public int getGreen() { return green; }
+        public int getBlue() { return blue; }
+
+        private int power() {
+            return red * green * blue;
         }
 
         private boolean isPossibleOutcome(GameSet other) {
             return this.red <= other.red && this.green <= other.green && this.blue <= other.blue;
         }
     }
+
 
     private static int regexFindNumber(Pattern patter, String s) {
         Matcher m = patter.matcher(s);
