815066c4717e1197b7af390ee25abc69cbf0e229
marcsieber <ms-coding@outlook.com>
2023-12-09 02:59:57 +0100

    Day 1 solved.

---
+++ src/main/java/common/Utils.java
@@ -0,0 +1,61 @@
+package common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+
+    public static List<String> readInputFromResources(String fileName)  {
+        List<String> lines = new ArrayList<>();
+
+        try (InputStream is = Utils.class.getClassLoader().getResourceAsStream(fileName)) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            while((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+            
+        } catch (IOException e) {
+            throw new IllegalArgumentException(String.format("Could not load file %s", fileName));
+        }
+
+        return lines;
+    }
+    
+    public static int smallestIndex(int [] array) {
+        if (array.length == 0) return -1;
+
+        int smallestIdx = 0;
+        long value      = array[smallestIdx];
+
+        for (int i = 0; i < array.length; i++) {
+            if (value > array[i]) {
+                value = array[i];
+                smallestIdx = i;
+            }
+        }
+        return smallestIdx;
+    }
+
+    public static int biggestIndex(int [] array) {
+        if (array.length == 0) return -1;
+
+        int biggestIdx = 0;
+        long value      = array[biggestIdx];
+
+        for (int i = 0; i < array.length; i++) {
+            if (value < array[i]) {
+                value = array[i];
+                biggestIdx = i;
+            }
+        }
+        return biggestIdx;
+    }
+
+
+}
---
+++ src/main/java/year2023/day1/Day1Solution.java
@@ -0,0 +1,94 @@
+package year2023.day1;
+
+import common.Utils;
+import jdk.jshell.execution.Util;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @see <a href="https://adventofcode.com/2023/day/1">Day 1</a>
+ */
+public class Day1Solution {
+
+//  private static final String inputFile = "day1-dummy.txt";
+    private static final String inputFile = "day1.txt";
+
+    private static final List<String> numbers = List.of(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    );
+
+    public static void main(String[] args) {
+
+        List<String> input = Utils.readInputFromResources(inputFile);
+
+        long total = input.stream()
+                .map(Day1Solution::preprocess)
+                .mapToLong(Day1Solution::readCalibrationValue1)
+                .sum();
+
+        System.out.println(total);
+    }
+
+    /**
+     * Preprocesses the string <code>s</code> by replacing the first letter of
+     * the number string with the corresponding number.
+     */
+    private static String preprocess(String s) {
+        int[] results1 = new int[9];
+        for (int i = 0; i < numbers.size(); i++) {
+            int idx = s.indexOf(numbers.get(i));
+            results1[i] = idx != -1 ? idx : Integer.MAX_VALUE;
+        }
+        int first = Utils.smallestIndex(results1);
+
+        int[] results2 = new int[9];
+        for (int i = 0; i < numbers.size(); i++) {
+            int idx = s.lastIndexOf(numbers.get(i));
+            results2[i] = idx != -1 ? idx : Integer.MIN_VALUE;
+        }
+        int second = Utils.biggestIndex(results2);
+
+        if (results1[first] != Integer.MAX_VALUE) {
+            int idx = results1[first];
+            s = s.substring(0, idx) + (first+1) + s.substring(idx+1);
+        }
+        if (results2[second] != Integer.MIN_VALUE) {
+            int idx = results2[second];
+            s = s.substring(0, idx) + (second+1) + s.substring(idx+1);
+        }
+        return s;
+    }
+
+
+    private static long readCalibrationValue1(String input) {
+        int zero = 48; // ascii 0
+
+        int first = 0;
+        for (int i = 0; i < input.length(); i++) {
+            int value = input.charAt(i) - zero;
+            if (0 <= value && value <= 9) {
+                first = value;
+                break;
+            }
+        }
+
+        int second = 0;
+        for (int i = input.length() - 1; i >= 0; i--) {
+            int value = input.charAt(i) - zero;
+            if (0 <= value && value <= 9) {
+                second = value;
+                break;
+            }
+        }
+        return first * 10 + second;
+    }
+
+
+
+
+
+}
