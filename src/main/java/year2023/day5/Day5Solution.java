c4b6f90ff41785f69e5f95960356a89bd082ad75
marcsieber <ms-coding@outlook.com>
2023-12-09 23:54:37 +0100

    Day 5: first riddle solved.

---
+++ src/main/java/year2023/day5/Day5Solution.java
@@ -0,0 +1,112 @@
+package year2023.day5;
+
+import common.Utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @see <a href="https://adventofcode.com/2023/day/5">Day 5</a>
+ */
+public class Day5Solution {
+
+//  private static final String inputFile = "day5-dummy.txt";
+    private static final String inputFile = "day5.txt";
+
+    private static final boolean debug = false;
+
+    public static void main(String[] args) {
+
+        // general
+        List<String> input = Utils.readInputFromResources(inputFile);
+        List<Long> seeds = Arrays.stream(input.get(0).split(":")[1].split(" "))
+                .filter(s -> ! s.isBlank())
+                .map(Long::parseLong)
+                .toList();
+
+        // first
+        PermutationSet set = createPermutationSet(input.subList(1, input.size()));
+        long nearestLocation = seeds.stream()
+                .map(set::resolve)
+                .min(Long::compareTo)
+                .orElse(0L);
+
+        System.out.println(nearestLocation);
+    }
+
+    private static PermutationSet createPermutationSet(List<String> lines) {
+        PermutationSet set = new PermutationSet();
+
+        for (String line : lines) {
+            if ( ! (line.isBlank() || line.contains("map"))) {
+                set.add(new Permutation(line));
+            } else if (line.contains("map")) {
+                set.newMapping();
+            }
+        }
+        return set;
+    }
+
+
+    private static class PermutationSet {
+
+        private final List<List<Permutation>> permutations = new ArrayList<>();
+
+        private void newMapping() {
+            permutations.add(new ArrayList<>());
+        }
+
+        private void add(Permutation permutation) {
+            permutations.get(permutations.size() -1).add(permutation);
+        }
+
+        private long resolve(long value) {
+            long result = value;
+            if (debug) { System.out.print(value); }
+
+            for (List<Permutation> perms : permutations) {
+                for (Permutation permutation : perms) {
+                    long permuted = permutation.resolve(result);
+                    if (result != permuted) {
+                        if (debug) { System.out.print(" -> " + permuted ); }
+                        result = permuted;
+                        break; // continue to the next set
+                    }
+                }
+            }
+            if (debug) { System.out.println(); }
+            return result;
+        }
+    }
+
+
+    private static class Permutation {
+
+        final long source;
+        final long start;
+        final long end;
+        final long delta;
+        final long size;
+
+        private Permutation (String line){
+            final String [] values = line.split(" ");
+
+            final long destination = Long.parseLong(values[0]);
+            source      = Long.parseLong(values[1]);
+            size        = Long.parseLong(values[2]);
+            delta       = destination - source;
+
+            start = source;
+            end   = source + size;
+        }
+
+        private long resolve(long value) {
+            if (start <= value && value < end ) {
+                value = value + delta;
+            }
+            return value;
+        }
+    }
+
+}
